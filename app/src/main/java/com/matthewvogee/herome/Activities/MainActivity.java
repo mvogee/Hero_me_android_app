e108632a899e1b2a145ab117afed40b61fdfc0be
Matthew Vogee <mvogee@yahoo.com>
2017-07-03 18:06:15 -0700

    initial commit. working on main activity layout

---
+++ app/src/main/java/com/matthewvogee/herome/Activities/MainActivity.java
@@ -0,0 +1,15 @@
+package com.matthewvogee.herome.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.matthewvogee.herome.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
